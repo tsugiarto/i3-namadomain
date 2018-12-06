7643af0e67485e818eee12d9b48a4c20a843a934
Tommy Sugiarto <t.sugiarto@icubic.co.id>
2018-12-06 10:17:18 +0700

    updates_06.11.18

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,42 @@ jQuery(document).ready(function($) {
 		removalDelay: 200
 	});
 
+	$('.js-home-slider').find('.slides').flickity({
+		autoPlay: 5000,
+		imagesLoaded: true,
+		wrapAround: true,
+	});
+
+	$('.js-client-slider').find('.slides').flickity({
+		imagesLoaded: true,
+		wrapAround: false,
+		pageDots: false,
+		contain: true,
+		groupCells: true,
+	});
+
+	$('.js-partner-slider').find('.slides').flickity({
+		imagesLoaded: true,
+		wrapAround: false,
+		pageDots: false,
+		contain: true,
+		groupCells: true,
+	});
+
+	$('.js-review-slider').find('.slides').flickity({
+		imagesLoaded: true,
+		wrapAround: false,
+		contain: true,
+		groupCells: true,
+		cellAlign: 'left',
+	});
+
+	$('.js-about-slider').find('.slides').flickity({
+		imagesLoaded: true,
+		wrapAround: true,
+		pageDots: false,
+	});
+
+	$('#mnsry_container').masonry();
+
 });
